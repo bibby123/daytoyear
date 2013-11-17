b221bd000bc0c1b9350aa6c1c994bd01c02c0928
bibby123 <123a123s123d@gmail.com>
2013-11-17 11:37:05 -0500

    Update DayToYear.cpp
    go off yourself you moronic cunt

--- DayToYear.cpp
+++ DayToYear.cpp
@@ -7,7 +7,7 @@ const string DayToYear::monthNames[12] = {"January", "February", "March", "April
 
 void DayToYear::print()
 {
-    cout << "Day " << day<< " would be \t" << monthNames[monthIndex] << " " << day << endl;
+    cout << "Day " << totalDay << " would be \t" << monthNames[monthIndex] << " " << day << endl;
 }
 
 DayToYear& DayToYear::operator++()
